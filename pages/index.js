575698c1a7c429155987b833a86110b24389e800
an-ger <aiboowen@gmail.com>
2021-01-20 12:39:39 +0800

    静态路由

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
-import {Button} from 'antd'
-
-export default () => <Button>Index</Button>
+import { Button } from "antd";
+import Router from 'next/router'
+import Link from "next/link";
+export default () => {
+    function gotoTestB () {
+        Router.push('/test/b')
+    }
+return (
+    <>
+    <Link href = "/a">
+    <Button>Index</Button>
+  </Link>
+  <Button onClick = {gotoTestB}>test b</Button>
+  </>
+)
+};
