7473eab6c3e4bd5b730c1794e2739b7e2c77246b
an-ger <aiboowen@gmail.com>
2021-01-20 11:33:10 +0800

    redis test

--- server.js
+++ server.js
@@ -5,24 +5,26 @@ const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-// app.prepare().then(() => {
+app.prepare().then(() => {
 const server = new Koa();
-const router = new Router();
-router.get("/test/:id", (ctx) => {
-    ctx.body = `<p>request /test${ctx.params.id}</p>`
-});
-// server.use(async(ctx, next) => {
-//     await handle(ctx.req, ctx.res)
-//     ctx.respond = false;
-// })
 server.use(async (ctx, next) => {
-  await next();
-});
-server.use(router.routes());
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false;
+  });
+// const router = new Router();
+// router.get("/test/:id", (ctx) => {
+//     // ctx.body = `<p>request /test${ctx.params.id}</p>`
+//     ctx.body = {success: true}
+//     ctx.set('Content-Type', 'application/json')
+// });
 // server.use(async (ctx, next) => {
-//     ctx.body = "<span>Koa Render2</span>";
-//   });
+//   await next();
+// });
+// server.use(async (ctx, next) => {
+//   await next();
+// });
+// server.use(router.routes());
 server.listen(3000, () => {
   console.log("fuck");
 });
-// })
+});
